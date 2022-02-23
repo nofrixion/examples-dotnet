3f786fa7c68fe43c020d016e76123c6600f58094
James Bramich <james.bramich@gmail.com>
2022-02-23 15:04:14 +1100

    added PR result method

---
+++ PaymentRequests/paymentrequests-id-result-get/Program.cs
@@ -0,0 +1,58 @@
+//-----------------------------------------------------------------------------
+// Description: Example of calling the NoFrixion MoneyMoov API paymentrequests/{id}/result 
+// GET method. It returns the result from the processing of a payment request.
+//
+// Usage:
+// 1. Create a MERCHANT access token in the sandbox portal at:
+//    https://portal-sandbox.nofrixion.com.
+// 2. Set the token as an environment variable in your console:
+//    set NOFRIXION_MERCHANT_TOKEN=<JWT token from previous step>
+/// 3. Run the applicatio using:
+//    dotnet run
+
+// 4. If successful the JSON object containing the payment request result data will be displayed.
+//-----------------------------------------------------------------------------
+
+using System.Net.Http.Json;
+
+const string baseUrl = "https://api-sandbox.nofrixion.com/api/v1/paymentrequests";
+
+// Payment requests use MERCHANT tokens (remember to keep these safe and secure).
+var jwtToken = Environment.GetEnvironmentVariable("NOFRIXION_MERCHANT_TOKEN");
+
+var client = new HttpClient();
+
+client.DefaultRequestHeaders.Add("Accept", "application/json");
+client.DefaultRequestHeaders.Add("Authorization", $"Bearer {jwtToken}");
+
+string paymentRequestID = "18fc90ae-0086-4ef3-8216-08d9f1deec34";
+
+try
+{
+    HttpResponseMessage response = await client.GetAsync($"{baseUrl}/{paymentRequestID}/result");
+    if (response.IsSuccessStatusCode)
+    {
+        // returns a paymentRequest
+        var prResult = await response.Content.ReadFromJsonAsync<PaymentRequestResult>();
+
+        // JSON object containing payment request result
+        Console.WriteLine(prResult);
+    }
+    else
+    {
+        // HTTP error codes will return a MoneyMoov API problem object
+        Console.WriteLine(await response.Content.ReadFromJsonAsync<ApiProblem>());
+    }
+
+
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Error: {e.Message}");
+}
+
+// type definition for response data
+record PaymentRequestResult(string paymentRequestID, string requestID, string transactionID, decimal amount, string currency,
+                string result, string status, string errorReason, string errorMessage, string cardTokenCustomerID,
+                string cardAuthorizationResponseID);
+record ApiProblem(string type, string title, int status, string detail);
